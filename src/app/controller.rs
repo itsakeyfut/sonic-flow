feb086194ac7b77eca9505ed25d8a69b87d042fc
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-08-09 13:08:43 +0900

    feat: implmements AppControlelr params

--- src/app/controller.rs
+++ src/app/controller.rs
@@ -1,11 +1,29 @@
 //! Main application controller
 
+use std::sync::Arc;
+
+use tracing::{info, debug, error};
+use tokio::sync::RwLock;
+
+use crate::audio::{AudioEngine, AudioEngineBuilder};
+use crate::config::ConfigManager;
 use crate::{Result, Error};
-use tracing::info;
+
+use super::{AppState, EventBus};
+use super::lifecycle::LifecycleManager;
 
 /// Main application controller that orchestrates all subsystems
 pub struct AppController {
-    _placeholder: (), // TODO: Add actual fields,
+    /// Audio engine for playback
+    audio_engine: Arc<RwLock<AudioEngine>>,
+    /// Configuration manager
+    config_manager: Arc<ConfigManager>,
+    /// Event bus for inter-component communication
+    event_bus: Arc<EventBus>,
+    /// Application state
+    app_state: Arc<RwLock<AppState>>,
+    /// Lifecycle manager
+    lifecycle_manager: LifecycleManager,
 }
 
 impl AppController {
