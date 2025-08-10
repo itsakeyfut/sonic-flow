52a0de92ef531bcd07aa238f3716fc2f2aa9f970
itsakeyfut <sicsempertyrannis98@gmail.com>
2025-08-10 19:19:01 +0900

    feat: Rect method - new

--- src/visualizer/traits.rs
+++ src/visualizer/traits.rs
@@ -242,3 +242,10 @@ impl Color {
         Self::rgb(r + m, g + m, b + m)
     }
 }
+
+impl Rect {
+    /// Create a new rectangle
+    pub fn new(x: f32, y: f32, width: f32, height: f32) -> Self {
+        Self { x, y, width, height }
+    }
+}
